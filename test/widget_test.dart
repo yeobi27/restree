443070888fc35dabcf797831d779f8c11af8f49b
yeobi27 <yeobi27@gmail.com>
2026-09-09 16:48:54 +0900

    widget_test.dart fixed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:restree/main.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:restree/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MaterialApp());
+    await tester.pumpWidget(const ProviderScope(child: RestreeApp()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
